ae12f29ce5b4211f562338f43708faf736f700d4
Hafizh Salam <hafizh.salam@ui.ac.id>
2023-12-04 08:12:31 +0700

    fix: query search

--- src/feat/cars/router.py
+++ src/feat/cars/router.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from core.db import db_engine
 from feat.cars.model import Car
+from feat.auth.router import get_user_id
 
 cars_router = APIRouter(prefix="/cars", tags=["cars"])
 
 
 @cars_router.get("/search")
 async def get_all_cars(
-    # user = Depends(),
+    user=Depends(get_user_id),
     q: str = "",
 ):
     with Session(db_engine) as session:
-        query = select(Car).where(Car.brand == q)
+        print(q)
+        query = select(Car).where(Car.brand.ilike(f"%{q}%"))
         cars = session.exec(query).all()
         return cars
